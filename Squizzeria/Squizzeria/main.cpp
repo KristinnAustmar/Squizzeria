bc3e14f51105b8a1f9b4c7d5c3fae9a1b1ccf912
KristinnAustmar <KristinnAustmar@desktop-b1mmcql.hir.is>
2017-11-28 17:17:10 +0000

    Breyting

---
+++ Squizzeria/Squizzeria/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Squizzeria
+//
+//  Created by   on 28/11/2017.
+//  Copyright © 2017 Kristinn. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Team Awesome!\n";
+    return 0;
+}
