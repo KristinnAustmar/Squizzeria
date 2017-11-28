fac8947b22cbe270d36b3299403fdf7cfbbe0c73
Kjartan97 <kjartan.reynir.olafsson@gmail.com>
2017-11-28 19:51:48 +0000

    added a main menu

---
+++ main_menu/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char manager, baker, salesperson, distributer;
+    manager = 'm';
+    baker = 'b';
+    salesperson = 's';
+    distributer = 'd';
+
+    cout << "||========================================|| " << endl;
+    cout << "||                 WELCOME                || " << endl;
+    cout << "||                                        || " << endl;
+    cout << "||========================================|| " << endl;
+    cout << "||                                        || " << endl;
+    cout << "||                                        || " << endl;
+    cout << "||        MANAGER         =>   m          || " << endl;
+    cout << "||                                        || " << endl;
+    cout << "||        BAKER           =>   b          || " << endl;
+    cout << "||                                        || " << endl;
+    cout << "||        CASHIER         =>   c          || " << endl;
+    cout << "||                                        || " << endl;
+    cout << "||        DISTRIBUTER     =>   d          || " << endl;
+    cout << "||                                        || " << endl;
+    cout << "||========================================|| " << endl;
+    cout << "||                                        || " << endl;
+    cout << "||        PLEASE STATE YOUR PURPOSE       || " << endl;
+
+    cin >>
+
+    return 0;
+}
